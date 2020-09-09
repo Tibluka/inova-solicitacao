00ee6502686e9d918a6d8aec3fefdfc2184aa162
Lucas Gomes <cprpc@hotmail.com>
2020-09-09 14:23:34 -0300

    responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'solicitacao', loadChildren: () => import('./pages/solicitacao-form/solicitacao-form.module').then(m => m.SolicitacaoFormModule) },
+  { path: '', loadChildren: () => import('./pages/solicitacao-form/solicitacao-form.module').then(m => m.SolicitacaoFormModule) },
   { path: 'consulta', loadChildren: () => import('./pages/consulta/consulta.module').then(m => m.ConsultaModule) },
   { path: 'finish/:id', loadChildren: () => import('./pages/finish/finish.module').then(m => m.FinishModule) }
 ];
