a713cc61a01792816b338bfaf67295af2267ce44
Lucas Gomes <cprpc@hotmail.com>
2020-09-14 18:43:00 -0300

    s

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'https://f2rkl509o9.execute-api.us-west-2.amazonaws.com/'
+  url: 'https://f2rkl509o9.execute-api.us-west-2.amazonaws.com/Dev'
 };
