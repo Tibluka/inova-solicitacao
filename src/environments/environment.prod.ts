dec0431f3603f60a7f2400ef9c3f82cd9d51757b
Lucas Gomes <cprpc@hotmail.com>
2020-09-22 20:04:46 -0300

    done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-      url: 'https://f2rkl509o9.execute-api.us-west-2.amazonaws.com/Dev'
+     /*  url: 'https://f2rkl509o9.execute-api.us-west-2.amazonaws.com/Dev' */
    
- /*  url: 'https://ut8oc1ljcf.execute-api.us-west-2.amazonaws.com/Prod' */
+   url: 'https://ut8oc1ljcf.execute-api.us-west-2.amazonaws.com/Prod' 
 
 };
